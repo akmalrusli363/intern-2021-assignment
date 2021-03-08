eb23fd271060aa597dd094f7dcc85714077213ff
Akmal <akmalrusli363@outlook.com>
2021-03-08 15:37:42 +0700

    Done Capitalization

--- src/main/java/com/intern/demo/Capitalization.java
+++ src/main/java/com/intern/demo/Capitalization.java
@@ -3,22 +3,27 @@ package com.intern.demo;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Capitalization {
 
-    private static String capitalize (String input) {
-        //TODO Add logic to capitalize String Input
-        //TODO Input = "I want to lEARN JAVA"
-        //TODO Output = "I Want To Learn Java"
-        return null;
+    private static String capitalize(String input) {
+        String[] words = input.toLowerCase(Locale.ROOT).split("\\s");
+        StringBuilder output = new StringBuilder();
+        for (String word : words) {
+            output.append(word.substring(0, 1).toUpperCase(Locale.ROOT)).append(word.substring(1)).append(" ");
+        }
+        return output.toString().trim();
     }
 
-    public static Object convertToMap (Object object) {
-        // TODO Add logic to capitalize Author and Title Of Books to Map with Key: Author, Value: List of Title Grouped By Author
-        // TODO INPUT: List<Book>
-        // TODO OUTPUT: Map<String, List<String>>
-        return null;
+    public static Map<String, List<String>> convertToMap(List<Book> inputs) {
+        for(Book input : inputs) {
+            input.setAuthor(capitalize(input.getAuthor()));
+            input.setTitle(capitalize(input.getTitle()));
+        }
+        return inputs.stream().collect(Collectors.groupingBy(Book::getAuthor, Collectors.mapping(Book::getTitle, Collectors.toList())));
     }
 }
